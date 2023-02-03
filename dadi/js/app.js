1df3a1650148ae3b47d7c7dea7f9e0e0ca814532
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-03 15:17:09 +0100

    EDIT JS generati numeri casuali

---
+++ dadi/js/app.js
@@ -0,0 +1,12 @@
+//GIOCO DEI DADI
+
+// 1.Chiedi all'utente di inserire il suo numero
+const numeroGiocatore = parseInt(prompt("Inserisci il tuo numero (1-6)"));
+
+// 2.Genera il numero casuale per il computer (math.floor+math.random )
+const numeroComputer = Math.floor(Math.random() * 6) + 1;
+
+
+
+
+
