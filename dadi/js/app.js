aec35c4ade46b03c985b5f5435ba30fa0743db9a
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-03 15:22:49 +0100

    EDIT JS confrontati i numeri stabilito risultato e vincitori

--- dadi/js/app.js
+++ dadi/js/app.js
@@ -3,8 +3,29 @@
 // 1.Chiedi all'utente di inserire il suo numero
 const numeroGiocatore = parseInt(prompt("Inserisci il tuo numero (1-6)"));
 
-// 2.Genera il numero casuale per il computer (math.floor+math.random )
+// 2.Genera il numero casuale per il computer (math.floor+math.random ossia da numeri che decrescono a numeri casuali )
 const numeroComputer = Math.floor(Math.random() * 6) + 1;
+
+//3.Confronta i due numeri 
+    //SE numgiocatore>numComputer 
+       //ALLORA giocatore ha vinto
+    //ALTRIMENTI SE numcomputer>numgiocatore
+       //ALLORA computer ha vinto
+let risultato;
+
+
+if (numeroGiocatore > numeroComputer) {
+  risultato = "Il giocatore ha vinto con il punteggio di " + numeroGiocatore;
+  console.log("Il giocatore ha vinto con il punteggio di " + numeroGiocatore);
+} else if (numeroComputer > numeroGiocatore) {
+  risultato = "Il computer ha vinto con il punteggio di " + numeroComputer;
+  console.log("Il computer ha vinto con il punteggio di " + numeroComputer);
+} else {
+  risultato = "Pareggio tra giocatore e computer con il punteggio di " + numeroGiocatore;
+  console.log("Pareggio tra giocatore e computer con il punteggio di " + numeroGiocatore);
+}
+
+
 
 
 
