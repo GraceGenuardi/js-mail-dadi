59d1b736a41f7907be176c6443cc110bb682fff2
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-03 13:56:41 +0100

    EDIT creato arrey email

---
+++ js/app.js
@@ -0,0 +1,6 @@
+console.log('Array')
+
+//1.Creiamo un arrey con una lista di email stringe al suo interno, in qaunto dato di archivazione di più valori al suo interno
+
+const email = ["jonsnow@gmail.com", "cesira@gmail.com", "sansa@gmail.com", "arya@gmail.com" ]
+console.log(email)
